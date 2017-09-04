0d1d2fbec75bd632446d42b48f0d7c9493f8e904
Ospens <1ospens1@gmail.com>
2017-09-04 22:47:30 +0300

    2

--- app/controllers/sltv_parser_controller.rb
+++ app/controllers/sltv_parser_controller.rb
@@ -1,21 +1,45 @@
 class SltvParserController < ApplicationController
 
 	def index
+		require 'open-uri'
+		require 'nokogiri'
+		require 'json'
 		html = Nokogiri::HTML(open("http://dota2.starladder.tv/tournaments")) 
 		#Регулярное выражение для поиска даты
-		date = '1 сентября '
+		date = '2 сентября '
 		@date = /^#{date}/
-		showings = []
+		@showings = []
+		@tags     = []
+		#Выбрать див с турнирами
+		@page = html.css('div.tournament_list')
 		@page.each do |doc|
 	 		doc.css('tr').each do |tr|
 				tags = tr.css('.count_tourney_teams').each do |tag|
-				next if tag.text.strip !~ @date %>
-				tr.css('.tournament_name').text.strip %>
-				page = tr.css('.tournament_name')[0]['href'].strip %>
-			end %>
-	<% end %>
-<% end %> 
-		@page = html.css('div.tournament_list')
+					#проверка даты регулярным выражением
+					next if tag.text.strip !~ @date
+					tour_name = tr.css('.tournament_name').text.strip
+					page = tr.css('.tournament_name')[0]['href'].strip
+
+					tour_link = Nokogiri::HTML(open('http://dota2.starladder.tv'+page+'/members'))
+					team_block = tour_link.css('div.tournament_members_list')					
+					team_block.each do |block|				
+						block.css('tr').each do |trr|
+							team_tag = trr.css('span.tourtment_match_info_am').text.strip
+							team_link = trr.css('.tournament_member')[0]['href']
+							@tags.push(
+								team_tag: team_tag,
+								team_link: team_link
+								)
+						end
+					end
+					@showings.push(
+					title: tour_name,
+					page: page, 
+					tour_tags: @tags
+					)
+				end
+			end
+		end 
 
 	end
 
