843175c69a4ad727235e67c80e1636760256231a
Ospens <1ospens1@gmail.com>
2017-09-28 22:50:18 +0300

    27.09

--- app/controllers/faceit_controller.rb
+++ app/controllers/faceit_controller.rb
@@ -22,33 +22,56 @@ class FaceitController < ApplicationController
 			@team_member4 = ''
 			@team_member5 = ''
 			@cap_steam    = ''
-
-			@team_url = "https://api.faceit.com/core/v1/teams/#{id}"
-			team = get_json(@team_url)
+			@country1     = ''
+			@country2     = ''
+			@country3     = ''
+			@country4     = ''
+			@country5     = ''
+			@team_url     = id
+			team = "https://api.faceit.com/core/v1/teams/#{id}"
+			team = get_json(team)
 			next if team['result'] == 'error'
 			#if team['payload'] != nil
 				@team_tag = team['payload']['name']
 				cap_uri = team['payload']['leader']
 				@cap_url = "https://api.faceit.com/core/v1/users/#{cap_uri}"
 				cap_link = get_json(@cap_url) if cap_uri != nil
+				@cap_nickname = cap_link['payload']['nickname'] if cap_link['payload']['nickname'] != nil
 				@cap_steam = cap_link['payload']['platforms']['steam'] if cap_link['payload']['platforms'] != nil
+				@cap_steam = cap_link['payload']['steam_id'] if cap_link['payload']['steam_id'] != nil
+				@cap_steam_64 = cap_link['payload']['steam_id_64'] if cap_link['payload']['steam_id_64'] != nil
+				@cap_country = cap_link['payload']['country'] if cap_link['payload']['country'] != nil
 				@team_member1 = team['payload']['members'][0]['nickname'] if team['payload']['members'] != nil
 				@team_member2 = team['payload']['members'][1]['nickname'] if team['payload']['members'][1] != nil
 				@team_member3 = team['payload']['members'][2]['nickname'] if team['payload']['members'][2] != nil
 				@team_member4 = team['payload']['members'][3]['nickname'] if team['payload']['members'][3] != nil
 				@team_member5 = team['payload']['members'][4]['nickname'] if team['payload']['members'][4] != nil
+				@cap_steam = cap_link['payload']['platforms']['steam'] if cap_link['payload']['platforms'] != nil
+				@country1 = team['payload']['members'][0]['country'] if team['payload']['members'] != nil
+				@country2 = team['payload']['members'][1]['country'] if team['payload']['members'][1] != nil
+				@country3 = team['payload']['members'][2]['country'] if team['payload']['members'][2] != nil
+				@country4 = team['payload']['members'][3]['country'] if team['payload']['members'][3] != nil
+				@country5 = team['payload']['members'][4]['country'] if team['payload']['members'][4] != nil
 			#end
 			break if count == 4  
 			count +=1
 		@teams.push(
 		team_tag: @team_tag,
 		team_url: @team_url,
-		cap_steam: @cap_steam, 
+		cap_nickname: @cap_nickname,
+		cap_steam: @cap_steam,
+		cap_country: @cap_country,
+		cap_steam_64: @cap_steam_64, 
 		team_member1: @team_member1,
 		team_member2: @team_member2,
 		team_member3: @team_member3,
 		team_member4: @team_member4,
-		team_member5: @team_member5
+		team_member5: @team_member5,
+		country1: @country1,
+		country2: @country2,
+		country3: @country3,
+		country4: @country4,
+		country5: @country5
 		)
 		end
 	end
