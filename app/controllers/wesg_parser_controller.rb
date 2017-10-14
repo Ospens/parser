3cab4a697cef7dacfca12033f1008cf152a961e5
Unknown <1ospens1@gmail.com>
2017-10-14 11:14:50 +0300

    wesg fix deleted team

--- app/controllers/wesg_parser_controller.rb
+++ app/controllers/wesg_parser_controller.rb
@@ -24,14 +24,25 @@ class WesgParserController < ApplicationController
 			next if @link == nil
 			@tag = tr.css('strong').text
 			@team_link = agent.get('https://en.wesg.com'+@link)
-			@country = @team_link.css('h2')[1].css('span')[0]['title']
-			@capitan_nick = @team_link.css('li.list-group-item strong')[0].text
-			@capitan_link = @team_link.css('li.list-group-item a')[0]['href']
-			@cap_link = agent.get('https://en.wesg.com'+@capitan_link)
-			@steam_id = @cap_link.css('h2 small').text
-			@skype = @cap_link.body.scan(/Skype:\n(.*)/).flatten[0].force_encoding("UTF-8")
-			@last_log_steam = agent.get('https://steamid.xyz/'+@steam_id)
-			@last_log_steam = @last_log_steam.body.scan(%r{<i>Last Logoff:</i>(.*)<br>})[0][0]
+			if @team_link.css('.alert-warning').present?
+				@country = 'команда удалена'
+				@capitan_nick = 'команда удалена'
+				@capitan_link = 'команда удалена'
+				@cap_link = 'команда удалена'
+				@steam_id = 'команда удалена'
+				@skype = 'команда удалена'
+				@last_log_steam = 'команда удалена'
+				@last_log_steam = 'команда удалена'
+			else
+				@country = @team_link.css('h2')[1].css('span')[0]['title']
+				@capitan_nick = @team_link.css('li.list-group-item strong')[0].text
+				@capitan_link = @team_link.css('li.list-group-item a')[0]['href']
+				@cap_link = agent.get('https://en.wesg.com'+@capitan_link)
+				@steam_id = @cap_link.css('h2 small').text
+				@skype = @cap_link.body.scan(/Skype:\n(.*)/).flatten[0].force_encoding("UTF-8")
+				@last_log_steam = agent.get('https://steamid.xyz/'+@steam_id)
+				@last_log_steam = @last_log_steam.body.scan(%r{<i>Last Logoff:</i>(.*)<br>})[0][0]
+			end
 			@showings.push(
 				tag: @tag,
 				team_link: @link,
