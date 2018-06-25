86a07aa4308c23b6235bf85f9cc7237b5a2918b2
Unknown <1ospens1@gmail.com>
2018-06-25 21:35:20 +0300

    fix faceit

--- app/controllers/faceit_controller.rb
+++ app/controllers/faceit_controller.rb
@@ -1,100 +1,106 @@
+# frozen_string_literal: true
+
 class FaceitController < ApplicationController
-	before_action :authenticate_user!
-   	respond_to :html, :js
+  before_action :authenticate_user!
+  respond_to :html, :js
 
-	def index	
-	end
+  def index; end
 
-	def parser_faceit
-		@teams = []
-		@members = []
-		return false if params[:q] == ''
-		respond = params[:q]
-		url = "https://s3.amazonaws.com/faceit-prod-frontend/tournaments_json/tournament_#{respond}_rankings.json"
-		uri = URI(url)
-		response = Net::HTTP.get(uri)
-		tour = JSON.parse(response)
-	
-		@agent = Mechanize.new { |agent|
-			agent.user_agent_alias = 'Mac Safari'
-			agent.request_headers = {'X-Requested-With' => 'XMLHttpRequest'}
-			}
+  def parser_faceit
+    @teams = []
+    @members = []
+    return false if params[:q] == ''
+    respond = params[:q]
+    url = "https://s3.amazonaws.com/faceit-prod-frontend/tournaments_json/tournament_#{respond}_rankings.json"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    tour = JSON.parse(response)
+    agent = auth 
+    tour['payload'].each do |id, team|
+      @team_member1 = ''
+      @team_member2 = ''
+      @team_member3 = ''
+      @team_member4 = ''
+      @team_member5 = ''
+      @cap_steam = cap_steam64 = 'Стима нет'
+      @country1     = ''
+      @country2     = ''
+      @country3     = ''
+      @country4     = ''
+      @country5     = ''
+      team = "https://api.faceit.com/core/v1/teams/#{id}"
+      team = get_json(team)
+      next if team['result'] == 'error'
+      team_pl = team['payload']
+      @team_tag = team_pl['name'] 
+      cap_uri = team_pl['leader']
+      @cap_url = "https://api.faceit.com/core/v1/users/#{cap_uri}"
+      cap_link = get_json(@cap_url) unless cap_uri.nil?
+      unless cap_link['result'] == 'error'
+        cap_pl = cap_link['payload']
+        @cap_nickname = cap_pl['nickname'] unless cap_pl['nickname'].nil?
+        @cap_steam = cap_pl['platforms']['steam'] unless cap_pl['platforms'].nil?
+        @cap_steam = cap_pl['steam_id'] unless cap_pl['steam_id'].nil?
+        unless cap_pl['steam_id_64'].nil?
+          cap_steam64 = cap_pl['steam_id_64']
+          @last_log = caps_steam(agent, cap_steam64)
+        end
+        @cap_country = cap_pl['country'] unless cap_pl['country'].nil?
+        @cap_steam = cap_pl['platforms']['steam'] unless cap_pl['platforms'].nil?
+      end
+      team_members = team_pl['members']
+      @team_member1 = team_members[0]['nickname'] unless team_members.nil?
+      @team_member2 = team_members[1]['nickname'] unless team_members[1].nil?
+      @team_member3 = team_members[2]['nickname'] unless team_members[2].nil?
+      @team_member4 = team_members[3]['nickname'] unless team_members[3].nil?
+      @team_member5 = team_members[4]['nickname'] unless team_members[4].nil?
+      @country1 = team_members[0]['country'] unless team_members.nil?
+      @country2 = team_members[1]['country'] unless team_members[1].nil?
+      @country3 = team_members[2]['country'] unless team_members[2].nil?
+      @country4 = team_members[3]['country'] unless team_members[3].nil?
+      @country5 = team_members[4]['country'] unless team_members[4].nil?
 
-		tour['payload'].each do |id, team|
-			#@team_tag = team['nickname']
-			@team_tag     = ''
-			@team_url     = ''
-			@team_member1 = ''
-			@team_member2 = ''
-			@team_member3 = ''
-			@team_member4 = ''
-			@team_member5 = ''
-			@cap_steam    = ''
-			@country1     = ''
-			@country2     = ''
-			@country3     = ''
-			@country4     = ''
-			@country5     = ''
-			@team_url     = id
-			team = "https://api.faceit.com/core/v1/teams/#{id}"
-			team = get_json(team)
-			next if team['result'] == 'error'
-				@team_tag = team['payload']['name']
-				cap_uri = team['payload']['leader']
-				@cap_url = "https://api.faceit.com/core/v1/users/#{cap_uri}"
-				cap_link = get_json(@cap_url) if cap_uri != nil
-				@cap_nickname = cap_link['payload']['nickname'] if cap_link['payload']['nickname'] != nil
-				@cap_steam = cap_link['payload']['platforms']['steam'] if cap_link['payload']['platforms'] != nil
-				@cap_steam = cap_link['payload']['steam_id'] if cap_link['payload']['steam_id'] != nil
-				@cap_steam_64 = cap_link['payload']['steam_id_64'] if cap_link['payload']['steam_id_64'] != nil
-				@cap_country = cap_link['payload']['country'] if cap_link['payload']['country'] != nil
-				@team_member1 = team['payload']['members'][0]['nickname'] if team['payload']['members'] != nil
-				@team_member2 = team['payload']['members'][1]['nickname'] if team['payload']['members'][1] != nil
-				@team_member3 = team['payload']['members'][2]['nickname'] if team['payload']['members'][2] != nil
-				@team_member4 = team['payload']['members'][3]['nickname'] if team['payload']['members'][3] != nil
-				@team_member5 = team['payload']['members'][4]['nickname'] if team['payload']['members'][4] != nil
-				@cap_steam = cap_link['payload']['platforms']['steam'] if cap_link['payload']['platforms'] != nil
-				@country1 = team['payload']['members'][0]['country'] if team['payload']['members'] != nil
-				@country2 = team['payload']['members'][1]['country'] if team['payload']['members'][1] != nil
-				@country3 = team['payload']['members'][2]['country'] if team['payload']['members'][2] != nil
-				@country4 = team['payload']['members'][3]['country'] if team['payload']['members'][3] != nil
-				@country5 = team['payload']['members'][4]['country'] if team['payload']['members'][4] != nil
-				
-				## переделать
-				last_log = SteamIdController.new
-				page = @agent.get('https://steamid.xyz/'+@cap_steam_64)
-				@last_log = last_log.get_last_log(page)
+      @members.push(
+        team_member1: @team_member1,
+        team_member2: @team_member2,
+        team_member3: @team_member3,
+        team_member4: @team_member4,
+        team_member5: @team_member5,
+        country1: @country1,
+        country2: @country2,
+        country3: @country3,
+        country4: @country4,
+        country5: @country5
+      )
+      @teams.push(team_tag: @team_tag,
+                  team_url: "https://www.faceit.com/ru/teams/#{id}",
+                  cap_nickname: @cap_nickname,
+                  cap_steam: @cap_steam,
+                  cap_country: @cap_country,
+                  cap_steam_64: cap_steam64,
+                  last_log: @last_log,
+                  members: @members)
+      @members = []
+    end
+  end
 
-				@members.push(
-					team_member1: @team_member1,
-					team_member2: @team_member2,
-					team_member3: @team_member3,
-					team_member4: @team_member4,
-					team_member5: @team_member5,
-					country1: @country1,
-					country2: @country2,
-					country3: @country3,
-					country4: @country4,
-					country5: @country5
-				)
-				@team_url = 'https://www.faceit.com/ru/teams/'+@team_url
-		@teams.push(
-		team_tag: @team_tag,
-		team_url: @team_url,
-		cap_nickname: @cap_nickname,
-		cap_steam: @cap_steam,
-		cap_country: @cap_country,
-		cap_steam_64: @cap_steam_64,
-		last_log: @last_log,
-		members: @members
-		)
-		@members = []
-		end
-	end
+  private
 
-	def get_json (url)
-		uri = URI(url)
-		response = Net::HTTP.get(uri)
-		return JSON.parse(response)
-	end
+  def caps_steam(agent, cap_steam64)
+    page = agent.get("https://steamid.xyz/#{cap_steam64}")
+    @last_log = SteamIdController.new.get_last_log(page)
+  end
+
+  def auth
+    Mechanize.new do |agent|
+      agent.user_agent_alias = 'Mac Safari'
+      agent.request_headers = { 'X-Requested-With' => 'XMLHttpRequest' }
+    end
+  end
+
+  def get_json(url)
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
 end
