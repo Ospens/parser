4dff691143ca1c66b002f59a415fbc0d3c4669f5
Unknown <1ospens1@gmail.com>
2018-10-31 07:51:03 +0300

    ke

--- app/controllers/vkontakte_controller.rb
+++ app/controllers/vkontakte_controller.rb
@@ -6,6 +6,6 @@ class VkontakteController < ApplicationController
     # создаем клиент
     #app = VkontakteApi::Client.new(client_id: 1, version: '5.85', access_token: 'cd4ce1b9ce519952faf2e62ba9e51b30ffc0d84b83c14ac035cec39be22aa83a2899795bd921060933c74')
  
-    render plain: 'rty'
+    render plain: '61025725'
   end
 end
