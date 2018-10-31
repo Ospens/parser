c929324ed99b50d711be1cd008246d3ca3120139
Unknown <1ospens1@gmail.com>
2018-10-31 07:48:57 +0300

    vk

---
+++ app/controllers/vkontakte_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class VkontakteController < ApplicationController
+  layout :false
+  def test
+    # создаем клиент
+    #app = VkontakteApi::Client.new(client_id: 1, version: '5.85', access_token: 'cd4ce1b9ce519952faf2e62ba9e51b30ffc0d84b83c14ac035cec39be22aa83a2899795bd921060933c74')
+ 
+    render plain: 'rty'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 	resources :steam_id
 	resources :faceit
 	resources :sltv_last_matches
-	resources :esl
+  resources :esl
+  
+  get "/test", to: "vkontakte#test"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
